c53c574c193792f0a0db84ed79dc9dcf66ebec72
Twentyquid <negativeradiations@gmail.com>
2023-11-24 18:54:10 +0530

    created new compoents and imported them

---
+++ src/components/TopPill.vue
@@ -0,0 +1,16 @@
+<script setup>
+import "../assets/toppill.scss";
+</script>
+
+<template>
+  <div class="top-pill">
+    <div class="image-circle"></div>
+    <div class="location">
+      Rajagiri <br />
+      Kakkanad
+    </div>
+    <div class="status">
+      <i class="ri-check-double-fill"></i>
+    </div>
+  </div>
+</template>
