14fb3b487e8511cff8ea565c64d8bf2929f175a9
bigbarin69 <prathik.prejith@gmail.com>
2023-11-24 13:30:51 +0530

    base loginpage

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,42 @@
+<script setup>
+import "../assets/loginpage.scss";
+import { ref } from "vue";
+
+const username = ref("");
+const password = ref("");
+
+function onUsername(e) {
+  username.value = e.target.value;
+}
+
+function onPassword(e) {
+  password.value = e.target.value;
+}
+
+function formValidate() {
+  console.log(username.value, password.value);
+}
+</script>
+
+<template>
+  <div class="loginpage-container">
+    <div class="ainat-title">AINAT</div>
+    <div class="login-container">
+      <form class="login-form">
+        <input
+          :value="username"
+          @input="onUsername"
+          placeholder="Username"
+          type="text"
+        />
+        <input
+          :value="password"
+          @input="onPassword"
+          placeholder="Password"
+          type="text"
+        />
+        <button @click="formValidate" type="button">Login!</button>
+      </form>
+    </div>
+  </div>
+</template>
