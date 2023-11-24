dca42358d69380812bf89033bed9a9dac82c350f
bigbarin69 <prathik.prejith@gmail.com>
2023-11-24 22:03:00 +0530

    pool update

--- src/components/DashboardPage.vue
+++ src/components/DashboardPage.vue
@@ -1,17 +1,40 @@
 <script setup>
 import "../assets/dashboard.scss";
+import HorizontalCams from "./HorizontalCams.vue";
+import VerticalCams from "./VerticalCams.vue";
 </script>
 
 <template>
   <div class="dashboard-container">
     <div class="pool-title-container"></div>
     <div class="pool-warning"></div>
-    <div class="main-pool-container"></div>
-    <div class="bottom-nav">
-      <input type="checkbox" class="toggle" />
-      <div class="ball-slider-container">
-        <div class="ball"></div>
+    <div class="main-pool-container">
+      <div class="horizontal-abs-cams-left">
+        <HorizontalCams />
       </div>
+      <div class="horizontal-abs-cams-right">
+        <HorizontalCams alertColor="red" alertBorder="black" />
+      </div>
+      <div class="vertical-abs-cams-top">
+        <VerticalCams />
+      </div>
+      <div class="vertical-abs-cams-bottom">
+        <VerticalCams alertColor="red" alertBorder="black" />
+      </div>
+      <div class="pool-rectangle">
+        <div class="grid-design">
+          <div class="grid-item"></div>
+          <div class="grid-item"></div>
+          <div class="grid-item"></div>
+          <div class="grid-item"></div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-nav">
+      <input type="checkbox" id="slider" class="toggle" />
+      <label for="slider" class="slider-container">
+        <div class="ball"></div>
+      </label>
     </div>
   </div>
 </template>
---
+++ src/components/HorizontalCams.vue
@@ -0,0 +1,27 @@
+<script>
+import "../assets/horizontalcams.scss";
+
+export default {
+  props: {
+    alertColor: String,
+    alertBorder: String,
+  },
+};
+</script>
+
+<template>
+  <!-- <div class="container"> -->
+  <div class="horizontal-cam-grid">
+    <div class="grid-item"><button></button></div>
+    <div class="grid-item"><button></button></div>
+    <div class="grid-item"><button></button></div>
+    <div class="grid-item"><button></button></div>
+    <div class="grid-item"><button></button></div>
+    <div class="grid-item">
+      <button
+        :style="{ backgroundColor: alertColor, borderColor: alertBorder }"
+      ></button>
+    </div>
+  </div>
+  <!-- </div> -->
+</template>
